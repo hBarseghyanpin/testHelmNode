f517d9ec5539f39096ded5801b6f4d8c4ce1bad4
hBarseghyanpin <h.barseghyan@pin-up.tech>
2023-07-17 03:13:07 +0400

    initial

---
+++ nodeFold/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+app.listen(port, () => {
+console.log('listening for request on port 8080');
+});
+app.get('/', (req, res) => {
+    console.log('request made / ');
+    res.send ("/");
+});
+app.get('/about', (req, res) => {
+    console.log('request made /about ');
+    res.send ("/about");
+});
+
+app.use((req, res) => {
+    console.log('request made /404 ');
+    res.send("404").statusCode(404);
+});
+
+app.get('/health-check',(req,res)=> {
+    console.log('Health pass');
+    res.send ("Health check passed");
+});
+app.get('/bad-health',(req,res)=> {
+    console.log('bad Health');
+    res.status(500).send('Health check did not pass');
+});
